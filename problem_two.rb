ce469cfcd399c38442b08acf4e2810f8ed3e72c9
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-09-26 16:44:12 +0300

    Two_REFACTOR: replace algorithm with built-in method

--- problem_two.rb
+++ problem_two.rb
@@ -2,16 +2,6 @@
 
 class ProblemTwo
   def max_two(array)
-    largest_one = 0
-    largest_two = 0
-    array.each do |value|
-      if value > largest_one
-        largest_two = largest_one
-        largest_one = value
-      elsif value > largest_two
-        largest_two = value
-      end
-    end
-    [largest_one, largest_two]
+    array.max(2)
   end
 end

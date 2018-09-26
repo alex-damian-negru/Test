c0997c6930ef36092bc6d34a6b557e4bb74fef6c
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-09-26 16:20:04 +0300

    Two_RED: should identify the largest two numbers

--- problem_two.rb
+++ problem_two.rb
@@ -1,1 +1,7 @@
 # Given an integer array of size N, identify the largest 2 numbers in this array. Do not sort the array.
+
+class ProblemTwo
+  def max_two(array)
+    return 0
+  end
+end
---
+++ spec/problem_two_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../problem_two'
+
+describe ProblemTwo do
+  it 'should identify the largest 2 numbers' do
+    test_input = [1, 7, 12, 3, 62, 26, 19, 128, 30, 64]
+    p_two = ProblemTwo.new
+    expect(p_two.max_two(test_input)).to eq([128, 64])
+  end
+end

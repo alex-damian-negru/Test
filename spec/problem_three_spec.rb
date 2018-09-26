2541f4e8e63671c21baa1cf36d7ada3d26ea9dae
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-09-26 22:38:12 +0300

    Three_RED: should determine if there are any duplicates

---
+++ spec/problem_three_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../problem_three'
+
+describe ProblemThree do
+  it 'should determine if there are any duplicates' do
+    test_input = [3, 4, 1, 6, 4, 1, 7, 9, 3, 10]
+    p_three = ProblemThree.new
+    expect(p_three.find_duplicates(test_input)).to eq('[3, 4, 1]')
+  end
+end

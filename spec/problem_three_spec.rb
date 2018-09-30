7a3bee551bd40b0f41284c7ae1fe8d7ff33042ba
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-09-30 21:57:39 +0300

    Three_REFACTOR: Improve test descriptions

--- spec/problem_three_spec.rb
+++ spec/problem_three_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../problem_three'
 
 describe ProblemThree do
-  it 'should determine if there are any duplicates' do
+  it 'should determine if there are duplicates' do
     test_input = [3, 4, 1, 6, 4, 1, 7, 9, 3, 10]
     p_three = ProblemThree.new
     expect(p_three.duplicates?(test_input)).to eq(true)
@@ -10,6 +10,6 @@ describe ProblemThree do
   it 'should not contain numbers greater than the array length' do
     test_input = [12, 4, 1, 6, 4, 1, 7, 9, 3, 10]
     p_three = ProblemThree.new
-    expect{p_three.duplicates?(test_input)}.to raise_error(RuntimeError)
+    expect { p_three.duplicates?(test_input) }.to raise_error(RuntimeError)
   end
 end

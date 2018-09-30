fe40fd561d166aab8dcab6a2cf651d75783c7269
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-09-30 20:37:03 +0300

    Two_GREEN: test negative numbers

--- spec/problem_two_spec.rb
+++ spec/problem_two_spec.rb
@@ -7,9 +7,15 @@ describe ProblemTwo do
     expect{p_two.max_two(test_input)}.to raise_error(RuntimeError)
   end
 
-  it 'should identify the largest 2 numbers' do
+  it 'should identify the largest 2 positive numbers' do
     test_input = [1, 7, 12, 3, 62, 26, 19, 128, 30, 64]
     p_two = ProblemTwo.new
     expect(p_two.max_two(test_input)).to eq([128, 64])
+  end
+
+  it 'should identify the largest 2 negative numbers' do
+    test_input = [-2, -71, -27, -8, -1]
+    p_two = ProblemTwo.new
+    expect(p_two.max_two(test_input)).to eq([-1, -2])
   end
 end

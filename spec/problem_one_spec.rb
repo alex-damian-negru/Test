642211318829259c6de4601ec259546b70ac2e38
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-09-30 21:25:10 +0300

    One_GREEN: Test for a set with negative numbers

--- spec/problem_one_spec.rb
+++ spec/problem_one_spec.rb
@@ -18,4 +18,10 @@ describe ProblemOne do
     test_input = [1, 3, 3, 7, 10, 15, 20, 15, 12, 1]
     expect(po.sum_even(test_input)).to eq(42)
   end
+
+  it 'should compute for a set with negative numbers' do
+    po = ProblemOne.new
+    test_input = [7, -2, 3, 16, -10, -21, 100, 57, 131]
+    expect(po.sum_even(test_input)).to eq(104)
+  end
 end

74695021df2ad640b9b8a705fe51ad1c2a4ac051
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-09-30 21:59:50 +0300

    Three_GREEN: Test for when there are no duplicates

--- spec/problem_three_spec.rb
+++ spec/problem_three_spec.rb
@@ -7,6 +7,12 @@ describe ProblemThree do
     expect(p_three.duplicates?(test_input)).to eq(true)
   end
 
+  it 'should determine if there are no duplicates' do
+    test_input = [1, 2, 3, 4, 5]
+    p_three = ProblemThree.new
+    expect(p_three.duplicates?(test_input)).to eq(false)
+  end
+
   it 'should not contain numbers greater than the array length' do
     test_input = [12, 4, 1, 6, 4, 1, 7, 9, 3, 10]
     p_three = ProblemThree.new

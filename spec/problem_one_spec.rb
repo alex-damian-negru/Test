e2524668768e522028cdaec646b0de695bc4de63
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-09-30 21:18:47 +0300

    One_REFACTOR: Improve test descriptions

--- spec/problem_one_spec.rb
+++ spec/problem_one_spec.rb
@@ -1,21 +1,21 @@
 require_relative '../problem_one'
 
 describe ProblemOne do
-  it 'should compute the sum of all even numbers' do
-    po = ProblemOne.new
-    test_input = [1, 3, 3, 7, 10, 15, 20, 15, 12, 1]
-    expect(po.sum_even(test_input)).to be 42
-  end
-
-  it 'should compute the sum of all even numbers for a set of even numbers' do
+  it 'should compute for a set of even numbers' do
     po = ProblemOne.new
     test_input = [2, 8, 12, 10, 128, 64, 32]
     expect(po.sum_even(test_input)).to eq(256)
   end
 
-  it 'should compute the sum of all even numbers for a set of odd numbers' do
+  it 'should compute for a set of odd numbers' do
     po = ProblemOne.new
     test_input = [1, 5, 11, 3, 1337, 9]
     expect(po.sum_even(test_input)).to eq(0)
+  end
+
+  it 'should compute for a set of mixed numbers' do
+    po = ProblemOne.new
+    test_input = [1, 3, 3, 7, 10, 15, 20, 15, 12, 1]
+    expect(po.sum_even(test_input)).to eq(42)
   end
 end

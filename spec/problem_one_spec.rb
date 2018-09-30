7c8413de8adbad1919c8b3c1a643713c3832333a
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-09-30 21:08:18 +0300

    One_GREEN: Test for a set of even numbers

--- spec/problem_one_spec.rb
+++ spec/problem_one_spec.rb
@@ -6,4 +6,10 @@ describe ProblemOne do
     test_input = [1, 3, 3, 7, 10, 15, 20, 15, 12, 1]
     expect(po.sum_even(test_input)).to be 42
   end
+
+  it 'should compute the sum of all even numbers for a set of even numbers' do
+    po = ProblemOne.new
+    test_input = [2, 8, 12, 10, 128, 64, 32]
+    expect(po.sum_even(test_input)).to eq(256)
+  end
 end

e8550b37b18702e4e6bb61526fca231e8de82050
Alex Negru Damian <negru.alex.damian@gmail.com>
2018-10-02 00:24:44 +0300

    Specs_REFACTOR: use let's in the test files

--- spec/problem_one_spec.rb
+++ spec/problem_one_spec.rb
@@ -1,32 +1,29 @@
 require_relative '../problem_one'
 
 describe ProblemOne do
+  let(:p_one) { $p_one = ProblemOne.new }
+
   it 'should compute for a set of even numbers' do
-    p_one = ProblemOne.new
     test_input = [2, 8, 12, 10, 128, 64, 32]
     expect(p_one.sum_even(test_input)).to eq(256)
   end
 
   it 'should compute for a set of odd numbers' do
-    p_one = ProblemOne.new
     test_input = [1, 5, 11, 3, 1337, 9]
     expect(p_one.sum_even(test_input)).to eq(0)
   end
 
   it 'should compute for a set of mixed numbers' do
-    p_one = ProblemOne.new
     test_input = [1, 3, 3, 7, 10, 15, 20, 15, 12, 1]
     expect(p_one.sum_even(test_input)).to eq(42)
   end
 
   it 'should compute for a set with negative numbers' do
-    p_one = ProblemOne.new
     test_input = [7, -2, 3, 16, -10, -21, 100, 57, 131]
     expect(p_one.sum_even(test_input)).to eq(104)
   end
 
   it 'should compute for an empty sum' do
-    p_one = ProblemOne.new
     test_input = []
     expect(p_one.sum_even(test_input)).to eq(0)
   end
--- spec/problem_three_spec.rb
+++ spec/problem_three_spec.rb
@@ -1,21 +1,20 @@
 require_relative '../problem_three'
 
 describe ProblemThree do
+  let(:p_three) { $p_three = ProblemThree.new }
+  
   it 'should determine if there are duplicates' do
     test_input = [3, 4, 1, 6, 4, 1, 7, 9, 3, 10]
-    p_three = ProblemThree.new
     expect(p_three.duplicates?(test_input)).to eq(true)
   end
 
   it 'should determine if there are no duplicates' do
     test_input = [1, 2, 3, 4, 5]
-    p_three = ProblemThree.new
     expect(p_three.duplicates?(test_input)).to eq(false)
   end
 
   it 'should not contain numbers greater than the array length' do
     test_input = [12, 4, 1, 6, 4, 1, 7, 9, 3, 10]
-    p_three = ProblemThree.new
     expect { p_three.duplicates?(test_input) }.to raise_error(RuntimeError)
   end
 end

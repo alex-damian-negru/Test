10b9c0be73f2506285ba04e3212e69b20948885b
Alex Negru Damian <negru.alex.damian@gmail.com>
2018-10-04 00:17:21 +0300

    Replace 'let' with 'subject' to DRY tests

--- spec/problem_three_spec.rb
+++ spec/problem_three_spec.rb
@@ -1,17 +1,15 @@
 require_relative '../problem_three'
 
 describe ProblemThree do
-  let(:p_three) { ProblemThree.new }
-
   it 'determines if there are duplicates' do
     set_with_duplicate = [3, 4, 1, 6, 4, 1, 7, 9, 3, 10]
-    duplicates = p_three.duplicates?(set_with_duplicate)
+    duplicates = subject.duplicates?(set_with_duplicate)
     expect(duplicates).to eq(true)
   end
 
   it 'determines if there are no duplicates' do
     set_no_duplicates = [1, 2, 3, 4, 5]
-    duplicates = p_three.duplicates?(set_no_duplicates)
+    duplicates = subject.duplicates?(set_no_duplicates)
     expect(duplicates).to eq(false)
   end
 end
--- spec/problem_two_spec.rb
+++ spec/problem_two_spec.rb
@@ -1,17 +1,15 @@
 require_relative '../problem_two'
 
 describe ProblemTwo do
-  let(:p_two) { ProblemTwo.new }
-
   it 'identifies the largest 2 positive numbers' do
     positive_input = [1, 7, 12, 3, 62, 26, 19, 128, 30, 64]
-    positive_largest = p_two.max_two(positive_input)
+    positive_largest = subject.max_two(positive_input)
     expect(positive_largest).to eq([128, 64])
   end
 
   it 'identifies the largest 2 negative numbers' do
     negative_input = [-2, -71, -27, -8, -1]
-    negative_largest = p_two.max_two(negative_input)
+    negative_largest = subject.max_two(negative_input)
     expect(negative_largest).to eq([-1, -2])
   end
 end

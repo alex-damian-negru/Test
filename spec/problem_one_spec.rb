93c1f7422dca6f49abdf0fbf5ea70e3a92198b85
Alex Negru Damian <negru.alex.damian@gmail.com>
2018-10-03 16:39:56 +0300

    Remove 'should' from tests

--- spec/problem_one_spec.rb
+++ spec/problem_one_spec.rb
@@ -3,27 +3,27 @@ require_relative '../problem_one'
 describe ProblemOne do
   let(:p_one) { $p_one = ProblemOne.new }
 
-  it 'should compute for a set of even numbers' do
+  it 'computes for a set of even numbers' do
     test_input = [2, 8, 12, 10, 128, 64, 32]
     expect(p_one.sum_even(test_input)).to eq(256)
   end
 
-  it 'should compute for a set of odd numbers' do
+  it 'computes for a set of odd numbers' do
     test_input = [1, 5, 11, 3, 1337, 9]
     expect(p_one.sum_even(test_input)).to eq(0)
   end
 
-  it 'should compute for a set of mixed numbers' do
+  it 'computes for a set of mixed numbers' do
     test_input = [1, 3, 3, 7, 10, 15, 20, 15, 12, 1]
     expect(p_one.sum_even(test_input)).to eq(42)
   end
 
-  it 'should compute for a set with negative numbers' do
+  it 'computes for a set with negative numbers' do
     test_input = [7, -2, 3, 16, -10, -21, 100, 57, 131]
     expect(p_one.sum_even(test_input)).to eq(104)
   end
 
-  it 'should compute for an empty sum' do
+  it 'computes for an empty sum' do
     test_input = []
     expect(p_one.sum_even(test_input)).to eq(0)
   end
--- spec/problem_two_spec.rb
+++ spec/problem_two_spec.rb
@@ -3,12 +3,12 @@ require_relative '../problem_two'
 describe ProblemTwo do
   let(:p_two) { $p_two = ProblemTwo.new }
 
-  it 'should identify the largest 2 positive numbers' do
+  it 'identifies the largest 2 positive numbers' do
     test_input = [1, 7, 12, 3, 62, 26, 19, 128, 30, 64]
     expect(p_two.max_two(test_input)).to eq([128, 64])
   end
 
-  it 'should identify the largest 2 negative numbers' do
+  it 'identifies the largest 2 negative numbers' do
     test_input = [-2, -71, -27, -8, -1]
     expect(p_two.max_two(test_input)).to eq([-1, -2])
   end
